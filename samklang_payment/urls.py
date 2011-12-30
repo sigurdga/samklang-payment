3b9b4a58ceea950f5a34078b6aeeb3c49f6594b7
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-12-30 23:03:35 +0100

    Update/Edit links are called "edit" in urls.py - make consistent

--- samklang_payment/urls.py
+++ samklang_payment/urls.py
@@ -4,7 +4,7 @@ from samklang_payment.views import *
 urlpatterns = patterns('',
         url(r'^campaign/$', DonationCampaignListView.as_view(), name='payment-campaign-list'),
         url(r'^campaign/(?P<slug>[-\w]+)/$', DonationCampaignDetailView.as_view(), name='payment-campaign-detail'),
-        url(r'^campaign/(?P<slug>[-\w]+)/edit/$', DonationCampaignUpdateView.as_view(), name='payment-campaign-update'),
+        url(r'^campaign/(?P<slug>[-\w]+)/edit/$', DonationCampaignUpdateView.as_view(), name='payment-campaign-edit'),
         url(r'^campaign/(?P<slug>[-\w]+)/donate/$', DonationCreateView.as_view(), name='payment-donation-create'),
         url(r'^campaign/(?P<campaign_slug>[-\w]+)/donated/(?P<slug>[-\w]+)/$', DonationDetailView.as_view(), name='payment-donation-detail'),
         url(r'^update/$', PaymentSiteUpdateView.as_view(), name='payment-site-update'),
