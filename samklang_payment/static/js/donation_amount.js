c90a011ea860d46ecda391aa14e6541f5ba55ac7
Sigurd Gartmann <sigurdga@sigurdga.no>
2012-01-03 21:50:56 +0100

    Prepare hotfix

--- samklang_payment/static/js/donation_amount.js
+++ samklang_payment/static/js/donation_amount.js
@@ -8,7 +8,7 @@ window.addEvent('domready', function() {
 
     $$("p.alt").each(function(el) {
         el.addEvent('click', function() {
-            val = el.get('text').replace(/\D/g, '');
+            val = el.get('text');
             document.id('id_amount').set('value', val);
             removeSelected();
             el.addClass("active");
