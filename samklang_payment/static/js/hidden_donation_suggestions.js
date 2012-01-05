d78258c5586f12d3246d0209881f1aaf2a434b5f
Sigurd Gartmann <sigurdga@sigurdga.no>
2012-01-05 10:09:01 +0100

    Strip off trailing \.00 in donation suggestions

--- samklang_payment/static/js/hidden_donation_suggestions.js
+++ samklang_payment/static/js/hidden_donation_suggestions.js
@@ -1,7 +1,7 @@
 var default_suggestion = document.id("id_amount").get('value');
 document.id("id_suggestions").get('value').split(/-/).each(function(e) {
     if (e != "") {
-        var element = new Element('p', {text: e, 'class': 'alt'});
+        var element = new Element('p', {text: e.replace(/\.00$/, ""), 'class': 'alt'});
         if (e == default_suggestion) { element.addClass("active"); }
         document.id("donation-alternatives").grab(element);
     }
