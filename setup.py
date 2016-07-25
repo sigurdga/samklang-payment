d073c08328169a2375b9d8c97aaf27e348521090
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-07-26 00:58:46 +0200

    0.6.0 released

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
         name='samklang-payment',
-        version="0.5.0",
+        version="0.6.0",
         author='Sigurd Gartmann',
         author_email='sigurdga-samklang@sigurdga.no',
         url='http://github.com/sigurdga/samklang-payment',
